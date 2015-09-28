95427c7f7ef1715567ca429cddef06f0a090bdd9
James Baca <james@jamesbaca.net>
2015-09-28 08:21:14 -0500

    Second attempt

--- euler_e2.ex
+++ euler_e2.ex
@@ -1,8 +1,7 @@
 defmodule Fib do
    def fib2 do
-      Stream.unfold({0, 1}, fn {a, b} -> {a, {b, a + b}} end)
+      Stream.unfold({0, 2}, fn {a, b} -> {a, {b, a+(4*b)}} end)
    end
 end
 
-
-IO.inspect Enum.reduce(Enum.filter(Fib.fib2 |> Enum.take(4000000), fn (x) -> rem(x,2) == 0 end), fn (x, acc) -> x + acc end) 
+IO.inspect Enum.reduce(Fib.fib2 |> Enum.take(12), fn (x, acc) -> x + acc end)
