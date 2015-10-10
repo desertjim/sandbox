f2c01a3b1e44c4346ba1a2490e53e204dc61dc03
James Baca <james@jamesbaca.net>
2015-10-10 05:45:05 -0500

    Another pass at e3

--- euler_e3.ex
+++ euler_e3.ex
@@ -1,16 +1,31 @@
 defmodule Prime do
-   def prime_factor(1, p, _) do
+   def prime_factor(_, 0, p, _) do
       p
    end
 
-   def prime_factor(n, p, acc) do
-      if rem(n, p) == 0 do
-         prime_factor(div(n,p), p + 2, p)
-      end
-      if rem(n, p) != 0 do
-         prime_factor(n, p + 2, acc)
-      end 
+   def prime_factor(_, 1, p, _) do
+      p
    end
+
+   def prime_factor(:true, n, p, _) do
+     newN = div(n,p)
+     prime_factor(div(newN,p) == newN / p, newN, p, p)
+   end
+
+   def prime_factor(:false, n, p, acc) do
+      truth = (div(n,p) == ( n / p))
+      p = case truth do
+         :true  -> p
+         :false -> p + 2
+      end
+
+      prime_factor(truth, n, p, acc)
+   end
+
+   def prime_factor(n) do
+       prime_factor(div(n,3) == n / 3 , n, 3, 1)
+   end
+
 end
 
-IO.puts Prime.prime_factor(600851475143, 3, [])
+IO.puts Prime.prime_factor(600851475143)
